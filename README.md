eb98af782cf254fc1ecc903fa685dc1f3043b77e
Andi Schröder <andreas.schroeder@iits-consulting.de>
2025-03-28 19:03:37 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TAST TASK
+
+An app for scrapping recipes with playwright from recipe pages, summarize them with a local llm via ollama api and extract all ingredients. After that a weekly meal plan can be created and an ingredient list can be exported to "To do"
---
+++ tasty-task-backend/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+val playwrightVersion = "1.51.0"
+
+val jacksonVersion = "2.18.3"
+val quarkusRestVersion = "3.21.0"
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("com.microsoft.playwright:playwright:$playwrightVersion")
+
+    implementation("io.quarkus:quarkus-rest:$quarkusRestVersion")
+    implementation("io.quarkus:quarkus-rest-jackson:$quarkusRestVersion")
+    implementation("io.quarkus:quarkus-rest-links:$quarkusRestVersion")
+
+    implementation("io.quarkus:quarkus-mongodb-panache")
+    implementation("io.quarkus:quarkus-mongodb-panache-kotlin")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+    implementation("io.quarkus:quarkus-arc")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "de.reinhardy.tastytask"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
